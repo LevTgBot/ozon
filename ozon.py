cfb744780ab713700121e66598a2a14ac6c1b2cd
Lev <161146668+LevTgBot@users.noreply.github.com>
2026-07-09 21:53:17 +0300

    Update ozon.py

--- ozon.py
+++ ozon.py
@@ -10,33 +10,33 @@ display.start()
 stealth = Stealth()
 
 with sync_playwright() as p:
-    with stealth.use_sync(p) as stealth_p:
-        browser = stealth_p.chromium.launch(
-        headless=False,
-        proxy={
-            "server": "http://93.77.191.156:8118"
-        },
-        args=[
-            "--disable-blink-features=AutomationControlled",
-            "--no-sandbox",             # Обязательно для root в Linux
-            "--disable-setuid-sandbox"  # Дополнительная изоляция для Linux
-        ]
-        )
+    stealth.use_sync(p)
+    browser = stealth_p.chromium.launch(
+    headless=False,
+    proxy={
+        "server": "http://93.77.191.156:8118"
+    },
+    args=[
+        "--disable-blink-features=AutomationControlled",
+        "--no-sandbox",             # Обязательно для root в Linux
+        "--disable-setuid-sandbox"  # Дополнительная изоляция для Linux
+    ]
+    )
     
-        context = browser.new_context(
-            viewport={'width': 1920, 'height': 1080},
-            user_agent="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36"
-        )
+    context = browser.new_context(
+        viewport={'width': 1920, 'height': 1080},
+        user_agent="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36"
+    )
     
-        page = browser.new_page()
-        print("Открываю Ozon...")
-        page.goto("https://www.ozon.ru/product/koshachya-myata-sharik-igrushki-dlya-koshek-3168418801/", wait_until="domcontentloaded")
-        while page.title() == "Antibot Challenge Page":
-            time.sleep(1)
-    
+    page = browser.new_page()
+    print("Открываю Ozon...")
+    page.goto("https://www.ozon.ru/product/koshachya-myata-sharik-igrushki-dlya-koshek-3168418801/", wait_until="domcontentloaded")
+    while page.title() == "Antibot Challenge Page":
         time.sleep(1)
-        #soup = BeautifulSoup(page.content(), 'html.parser')
-        #elements = soup.find_all('span', class_='tsHeadline600Large')
-        print(page.content())
-        browser.close()
-        display.stop()
+    
+    time.sleep(1)
+    #soup = BeautifulSoup(page.content(), 'html.parser')
+    #elements = soup.find_all('span', class_='tsHeadline600Large')
+    print(page.content())
+    browser.close()
+    display.stop()
