b03d7cbb36ab63497f57baf6407e510c34aa7d6d
Lev <161146668+LevTgBot@users.noreply.github.com>
2026-07-09 22:03:37 +0300

    Update ozon.py

--- ozon.py
+++ ozon.py
@@ -1,45 +1,19 @@
-from playwright.sync_api import sync_playwright
-from playwright_stealth import Stealth 
-import time
-from bs4 import BeautifulSoup
-from pyvirtualdisplay import Display
-import os
+from camoufox.sync_api import Camoufox
 
 display = Display(visible=0, size=(1920, 1080))
 display.start()
 
-stealth = Stealth()
 
-with sync_playwright() as p:
-    user_data_dir = os.path.join(os.getcwd(), "user_data")
-    stealth.use_sync(p)
-    browser = p.chromium.launch(
+
+# Camoufox сам сгенерирует идеальные отпечатки реального пользователя
+with Camoufox(
     headless=False,
-    no_viewport=True,
-    proxy={
-        "server": "http://93.77.191.156:8118"
-    },
-    args=[
-        "--disable-blink-features=AutomationControlled",
-        "--no-sandbox",             # Обязательно для root в Linux
-        "--disable-setuid-sandbox"  # Дополнительная изоляция для Linux
-    ]
-    )
-    
-    context = browser.new_context(
-        viewport={'width': 1920, 'height': 1080},
-        user_agent="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36"
-    )
-    
+    proxy={"server": "http://ip:port"}
+) as browser:
     page = browser.new_page()
-    print("Открываю Ozon...")
-    page.goto("https://www.ozon.ru/product/koshachya-myata-sharik-igrushki-dlya-koshek-3168418801/", wait_until="domcontentloaded")
-    while page.title() == "Antibot Challenge Page":
-        time.sleep(1)
-    
-    time.sleep(1)
-    #soup = BeautifulSoup(page.content(), 'html.parser')
-    #elements = soup.find_all('span', class_='tsHeadline600Large')
-    print(page.content())
-    browser.close()
+    page.goto("https://ozon.ru")
+
+    print(page.title())
+    page.wait_for_timeout(10000)
+
     display.stop()
