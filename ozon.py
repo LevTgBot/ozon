dfc1cbabe419063d474335e4446ecff3775cb78a
Lev <161146668+LevTgBot@users.noreply.github.com>
2026-07-09 15:03:09 +0300

    Update ozon.py

--- ozon.py
+++ ozon.py
@@ -3,10 +3,15 @@ import time
 from bs4 import BeautifulSoup
 
 with sync_playwright() as p:
-    browser = p.chromium.launch(
-        headless=False,
-        args=["--disable-blink-features=AutomationControlled"]
+   browser = p.chromium.launch(
+    headless=True,  # Для работы на сервере без экрана
+    args=[
+        "--disable-blink-features=AutomationControlled",
+        "--no-sandbox",             # Обязательно для root в Linux
+        "--disable-setuid-sandbox"  # Дополнительная изоляция для Linux
+    ]
     )
+
     context = browser.new_context(
         viewport={'width': 1920, 'height': 1080},
         user_agent="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36"
