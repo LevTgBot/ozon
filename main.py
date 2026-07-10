2d45a90adb3230de0dc26bbf2b6309178e4518af
Lev <161146668+LevTgBot@users.noreply.github.com>
2026-07-10 08:58:39 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,36 @@
+from os import getenv
+import asyncio
+from aiogram import Bot, Dispatcher, Router
+from aiogram.filters import Command
+from dotenv import load_dotenv
+from aiogram.types import (
+    Message
+)
+
+load_dotenv()
+
+TOKEN = getenv("BOT_TOKEN")
+router = Router()
+
+@router.message(Command("start"))
+async def start(message: Message):
+    await message.answer("Введи ссылку на товар.")
+
+@router.message()
+async def menu(message: Message):
+    await message.answer("Ищу товар:", message)
+
+
+dp = Dispatcher()
+dp.include_routers(router)
+
+
+async def main():
+    bot = Bot(token=TOKEN)
+
+    print("Start")
+
+    await dp.start_polling(bot)
+
+if __name__ == "__main__":
+    asyncio.run(main())
