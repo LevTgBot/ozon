eba4a2bd073d35f418f94cb25112501888853d97
Lev <161146668+LevTgBot@users.noreply.github.com>
2026-07-10 08:58:00 +0300

    Update ozon.py

--- ozon.py
+++ ozon.py
@@ -3,26 +3,27 @@ from pyvirtualdisplay import Display
 import time
 from bs4 import BeautifulSoup
 
-display = Display(visible=0, size=(1920, 1080))
-display.start()
+def price(url):
+    display = Display(visible=0, size=(1920, 1080))
+    display.start()
 
 
 
-# Camoufox сам сгенерирует идеальные отпечатки реального пользователя
-with Camoufox(
-    headless=False,
-    proxy={"server": "http://93.77.191.156:8118"},
-    geoip=True
-) as browser:
-    page = browser.new_page()
-    page.goto("https://www.ozon.ru/product/koshachya-myata-sharik-igrushki-dlya-koshek-3168418801/")
-    while page.title() == "Antibot Challenge Page":
-        time.sleep(1)
+    # Camoufox сам сгенерирует идеальные отпечатки реального пользователя
+    with Camoufox(
+        headless=False,
+        proxy={"server": "http://93.77.191.156:8118"},
+        geoip=True
+    ) as browser:
+        page = browser.new_page()
+        page.goto("https://www.ozon.ru/product/koshachya-myata-sharik-igrushki-dlya-koshek-3168418801/")
+        while page.title() == "Antibot Challenge Page":
+            time.sleep(1)
         
-    print(page.title())
-    time.sleep(1)
-    soup = BeautifulSoup(page.content(), 'html.parser')
-    elements = soup.find_all('span', class_='tsHeadline600Large')
-    print("Цена:", elements[0].text)
+        print(page.title())
+        time.sleep(1)
+        soup = BeautifulSoup(page.content(), 'html.parser')
+        elements = soup.find_all('span', class_='tsHeadline600Large')
+        print("Цена:", elements[0].text)
 
-    display.stop()
+        display.stop()
